86c87aa123af285c5119483be0867e4e367b302e
Ross Frank <rossfrank27@gmail.com>
2018-08-01 22:39:11 -0400

    update draft channel
    code cleanup

--- config-example.py
+++ config-example.py
@@ -4,7 +4,8 @@ api_key = ''
 league_id = ''
 year = ''
 server = ''
+draft_channel = ''
 background_channel = ''
 #remove positions you want to use
 bad_positions = ['TMWR', 'TMRB', 'TMDL', 'TMLB', 'TMDB', 'TMTE', 'ST', 'Off', 'TMQB', 'TMPK', 'TMPN', 'Coach', 'PN', 'Def', 'PK', 'RB', 'QB', 'DE', 'CB', 'LB', 'WR', 'TE', 'S', 'DT']
-discord_name = {'0001': '@', '0002': '@', '0003': '@', '0004': '@', '0005': '@', '0006': '@', '0007': '@', '0008': '@', '0009': '@', '0010': '@'}
+discord_name = {'0001': '', '0002': '', '0003': '', '0004': '', '0005': '', '0006': '', '0007': '', '0008': '', '0009': '', '0010': ''
